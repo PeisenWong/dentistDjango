7955fe9cf2c75319f76d636fe2dc948ced7f59bb
PeisenWong <wongpeisen@gmail.com>
2021-07-10 21:05:23 +0800

    debug

--- dentist/settings.py
+++ dentist/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&^v@$+lo#xn_s2lt)@qb@+lzf_t#os)0cvsko-yrhbomrmwjjn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
